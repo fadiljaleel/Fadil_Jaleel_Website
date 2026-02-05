4966cf04c2a2f0dbe0e114754628d0a3c9707a5a
Muhammed Fadil <fadiljaleel@gmail.com>
2026-02-05 12:13:53 +0400

    Update head.tsx: add black logo for Google search and structured data

--- app/head.tsx
+++ app/head.tsx
@@ -1,22 +1,29 @@
+import Script from "next/script"
+
 export default function Head() {
   return (
     <>
-      <link
-        rel="icon"
-        type="image/x-icon"
-        sizes="16x16"
-        href="/icon.ico"
-      />
-      <link
-        rel="icon"
-        type="image/x-icon"
-        sizes="32x32"
-        href="/icon.ico"
-      />
-      <link
-        rel="shortcut icon"
-        href="/icon.ico"
-      />
+      {/* Favicons */}
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+      {/* Viewport */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      {/* Structured Data for Google Search Logo */}
+      <Script id="structured-data" type="application/ld+json">
+        {`
+        {
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "name": "Muhammed Fadil",
+          "url": "https://www.fadiljaleel.com",
+          "logo": "https://www.fadiljaleel.com/images/logo-black.png"
+        }
+        `}
+      </Script>
     </>
   )
-} 
+}
